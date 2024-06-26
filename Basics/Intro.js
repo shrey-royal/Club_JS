aa1c80833130da43834ab04a71e05bce168d4bbe
Shrey K <srkkadia@gmail.com>
2024-06-26 19:06:35 +0530

    Intro to JS

---
+++ Basics/Intro.js
@@ -0,0 +1,12 @@
+/*
+ECMASCRIPT: The core lang that was used to interact with the websites within the web browser
+
+Client-side JS -> Used on the browser (simple js)
+Server-side JS -> Node.js, express.js
+
+JS
+
+*/
+
+naam = prompt("Enter your name: ")
+console.log("Hello,", naam);
