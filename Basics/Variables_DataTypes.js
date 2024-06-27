2e557adc1eabdfb7cc6be8ad00ec49e72488f88d
Shrey K <srkkadia@gmail.com>
2024-06-27 19:06:01 +0530

    variables and data-types (intro)

---
+++ Basics/Variables_DataTypes.js
@@ -0,0 +1,50 @@
+console.log("Hello, World!")
+
+// document.write("Hello, " + prompt("Enter your name: "))
+
+/*
+var - variable 
+let - used to declare the variable
+const - to declare a varible as a constant
+
+error-1: undefined (when the var is not intialized)
+error-2: undeclared (when the var is not declared) (ReferenceError)
+
+*/
+
+// const message = "Welcome to JS";
+// message = "Welcome home"    // (TypeError)
+// console.log(message)
+
+// let abc = "Traingle"
+// console.log(abc);
+
+
+// {
+//     let java = "Steve"
+// }
+// console.log("Everybody say Hello to " + java);
+
+
+/*
+Data Types:
+
+String      - "collection", "of", "characters", "etc."
+Number      - 420, etc.
+Bigint      - >(2^53-1) (non-existent)
+Boolean     - true (other than 0), false (0)
+Undefined   - undefined
+Null        - null(no memory)
+Symbol      - a class to create our own specific constant
+Object
+
+The Object Datatype
+The object data type can contain both built-in objects, and user defined objects:
+
+Built-in object types can be:
+objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
+
+User-Defined object types can be:
+etc.
+
+*/
