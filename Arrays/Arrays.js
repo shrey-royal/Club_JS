d0d24e037ab7b886463abef3b8ed9c9cadd4a918
Shrey K <srkkadia@gmail.com>
2024-07-07 18:04:11 +0530

    1d Array methods

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -32,6 +32,79 @@ Arrays:
 
 // in JS array may have data that belongs to multiple data type 
 
-var arr = [12, 23, 4.23, 45.23, "name", "another name"]
+// var arr = [12, 23, 4.23, 45.23, "name", "another name"]
 
-console.log(arr);
+// console.log(arr);
+
+// ----------------------------------------------------------------------------------
+
+// Array methods
+
+var veggies = [
+    "Bitter Gourd (Karela)",
+    "Bottle Gourd (Lauki)",
+    "Ridge Gourd (Turai)",
+    "Snake Gourd (Galka)",
+    "Pointed Gourd (Parwal)",
+    "Drumstick (Moringa)",
+    "Taro Root (Arbi)",
+    "Elephant Foot Yam (Suran)",
+    "Ash Gourd (Petha)",
+    "Ivy Gourd (Tindora)",
+    "Fenugreek Leaves (Methi)",
+    "Amaranth Leaves (Patra)",
+    "Colocasia Leaves (Arbi ke Patte)",
+    "Beet Greens (Chukandar ke Patte)",
+    "Mustard Greens (Sarson)",
+    "Radish Greens (Mooli ke Patte)",
+    "Spinach (Palak)",
+    "Fenugreek Seeds Sprouts (Methi Dana)",
+    "Cluster Beans (Gawar Phali)",
+    "French Beans",
+    "Yardlong Beans (Bora)",
+    "Broad Beans (Sem)",
+    "Green Peas (Matar)",
+    "Corn (Makai)",
+    "Bamboo Shoots (Karil)",
+    "Jackfruit (Kathal)",
+    "Raw Banana (Kachcha Kela)",
+    "Green Papaya (Kachcha Papita)",
+    "Indian Gooseberry (Amla)",
+    "Curry Leaves (Kadi Patta)",
+    "Raw Mango (Keri)",
+    "Sponge Gourd (Nenua)",
+    "Brinjal (Baingan)",
+    "Lady Finger (Bhindi)",
+    "Cabbage (Patta Gobi)",
+    "Cauliflower (Phool Gobi)",
+    "Radish (Mooli)",
+    "Turnip (Shalgam)",
+    "Carrot (Gajar)",
+    "Tomato (Tamatar)"
+]
+
+// console.log(veggies);
+
+// console.log(`Length: ${ veggies.length }`);
+
+// var ret = veggies.push("New Element", "new element 1")
+// console.log(ret);
+
+// console.log(veggies.pop()); //remove last
+// console.log(veggies.shift()); //remove first
+// console.log(veggies.unshift("new element")); //add element at start
+
+// console.log(veggies.slice(0, 5));
+// console.log(veggies.splice(0, 5, "new element 1", "new element 2", "new element 3"));
+
+// var arr = []
+// veggies.fill(10, 2, 5);
+// console.log(veggies[5], veggies);
+
+// console.log(veggies.toString());
+
+// console.log(veggies.at(10));
+
+// var arr = [0, 1]
+// console.log(arr.concat(veggies));
+
