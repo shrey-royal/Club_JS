69a88c9027153960bd9774d568ec4dc690b1a238
Shrey K <srkkadia@gmail.com>
2024-07-23 18:55:29 +0530

    Arrow fn, Anonymous FN

---
+++ Functions/Fun2.js
@@ -0,0 +1,35 @@
+// Arrow Function
+
+// const greet = () => {
+//     console.log("Hello, World!");
+//     return;
+// }
+// greet();
+
+// var add = (a, b) => a + b
+// console.log(add(2, 3));
+
+// var product = (n1, n2) => console.log(`${n1} * ${n2} = ${n1*n2}`)
+// product(100, 3)
+
+// var prettyprint = msg => console.log(`💫${msg}✨`)
+// prettyprint("Hello, World!")
+
+//Anonymous Function
+
+// setTimeout(function() {
+//     console.log("This is 1 sec");
+// }, 1000)
+
+// console.log('Fetching Tomorrow\'s Weather');
+
+// var data = +prompt("data?");
+
+// setTimeout(() => {
+//     if(data === 1) {
+//         alert("Data fetched successfully!");
+//     } else {
+//         alert("failed to fetch data!");
+//     }
+// }, 1000);
+
