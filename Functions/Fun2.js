8524d00c9c4f3a5a3dfcda8b8d8d1785338156f9
Shrey K <srkkadia@gmail.com>
2024-07-24 19:13:09 +0530

    Generator Functions

--- Functions/Fun2.js
+++ Functions/Fun2.js
@@ -33,3 +33,97 @@
 //     }
 // }, 1000);
 
+//IIFE Function (Immediately Invoked Function Expression)
+
+// (function() {
+//     console.log('This is an IIFE');
+// })();
+
+// var square = (function(num) {
+//     return num*num;
+// })(+prompt("Enter any number: "));
+
+// console.log(square);
+
+// a = 2;
+
+// (function(num) {
+//         console.log(num**3);
+//     })(a);
+    
+//generator Function
+
+// function* fn() {
+//     yield 'First';
+//     yield 'Second';
+//     yield 'Third';
+// }
+
+// var gen = fn();
+
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+
+// for (const i of gen) {
+//     console.log(i);
+// }
+
+// function* fn1() {
+//     const f = yield 'First';
+//     console.log(f);
+//     const s = yield 'Second';
+//     console.log(s);
+//     const t = yield 'Third';
+//     console.log(t);
+// }
+
+// var gen = fn1();
+
+// console.log(gen.next());
+// console.log(gen.next("Hello, World!"));
+
+
+// function* fn2() {
+//     yield 1;
+//     yield 2;
+//     yield 'three';
+//     yield 4;
+//     yield 5;
+//     return 'Done';
+// }
+
+// var gen = fn2();
+
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+// console.log(gen.next().value);
+
+
+function All() {
+    var n1 = +prompt("Enter n1: ");
+    var n2 = +prompt("Enter n2: ");
+
+    return n1+n2;
+}
+
+function* fn2() {
+    yield 1;
+    yield 2;
+    yield All();
+    yield 4;
+    yield 5;
+    return 'Done';
+}
+
+var gen = fn2();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next());
